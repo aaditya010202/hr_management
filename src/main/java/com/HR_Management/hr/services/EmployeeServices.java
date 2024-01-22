99176f1f724e417fa038233612fbd3d5c54d43b8
aaditya010202 <aadibhatnagar535@gmail.com>
2024-01-22 17:16:59 +0530

    third commit

--- src/main/java/com/HR_Management/hr/services/EmployeeServices.java
+++ src/main/java/com/HR_Management/hr/services/EmployeeServices.java
@@ -140,6 +140,7 @@ private EmployeeRequestDTO convertEntityToDtoRequest(Employee employee)
             employeeRepository.deleteById(empId);
         }
     }
+
     public Employee updateEmployeeById(String empId, Employee employeeData)
     {
             Optional<Employee> optionalEmployee = employeeRepository.findById(empId);
